89a1f3c9c3a8c39a0d92a3baf48bf2c627accb87
satyajeet <satyajeet@kreditz.com>
2026-03-10 12:48:26 +0530

    update port setting

--- backend/server.js
+++ backend/server.js
@@ -39,4 +39,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+console.log('Starting server...');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on 0.0.0.0:${PORT}`);
+});
