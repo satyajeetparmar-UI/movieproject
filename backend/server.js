a7ac7e277921249e0b24469ba74833ec8c788991
Satyajeet Parmar <65767924+satyajeetparmar-UI@users.noreply.github.com>
2026-03-17 13:33:03 +0530

    remove nodeenvmode

--- backend/server.js
+++ backend/server.js
@@ -41,5 +41,5 @@ const PORT = process.env.PORT || 5000;
 
 console.log('Starting server...');
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on 0.0.0.0:${PORT}`);
+  console.log(`Server running in mode on ${PORT}`);
 });
